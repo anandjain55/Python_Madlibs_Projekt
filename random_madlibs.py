58d9e4b22e27ee852acb57fcf468cfb99630462c
anandjain55 <92063959+anandjain55@users.noreply.github.com>
2021-12-19 18:10:36 +0100

    madlibs files

---
+++ random_madlibs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from sample_madlibs import hp, code, zombie, hungergames
+import random
+
+if __name__ == "__main__":
+    m = random.choice([hp, code, zombie, hungergames])
+    m.madlib()
+© 2021 GitHub, Inc.
+Terms
+
